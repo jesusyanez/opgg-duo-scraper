2250631fd78e461b2a6323ae90c3cc7fbdb7233a
jesusyanez <jryanez@jryanez.com>
2022-05-12 17:48:31 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# opgg-duo-scraper
+# About
+
+A promised based op.gg scraper for League of Legends but with 'recently played 
+with' section.
+
+
+## Usage
+```javascript
+const opggDuoScraper = require('opgg-duo-scraper');
+
+opggDuoScraper.getStats('yassuo', 'na', false).
+    then(stats => console.log(stats))       
+```
+
+
+```javascript
+// Output
+{
+  Name: 'yassuo',
+  Level: '386',
+  ProfilePic: 'https://opgg-static.akamaized.net/images/profile_icons/profileIcon1413.jpg?image=q_auto&image=q_auto,f_png,w_auto&v=1652335124059',
+  RecentlyPlayedWith: [
+    [ 'Summoner', 'Played', 'Win', 'Lose', 'Win Ratio' ],
+    [ 'skb', '3', '2', '1', '67%' ],
+    [ 'POOGERS', '3', '2', '1', '67%' ],
+    [ '1083', '2', '0', '2', '0%' ],
+    [ 'Chimp', '2', '2', '0', '100%' ],
+    [ 'evergreenily', '2', '2', '0', '100%' ],
+    [ 'Heat Waves', '2', '2', '0', '100%' ],
+    [ 'Plat Players Suc', '2', '0', '2', '0%' ],
+    [ 'EmbraceLife', '2', '1', '1', '50%' ],
+    [ 'TwTv TheRealRTO', '2', '1', '1', '50%' ]
+  ]
+}
+```
+
+## Paramaters
+getStats(username: string, region: string, refresh: boolean)
+
+
+## Regions
+
+na kr oce jp euw eune lan br las ru tr
+
+
---
+++ index.js
@@ -0,0 +1,55 @@
+const puppeteer = require('puppeteer-extra');
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
+puppeteer.use(AdblockerPlugin());
+
+
+
+
+exports.getStats = async (user, region, refresh) => {
+    try {
+        const browser = await puppeteer.launch({ headless: true });
+        const context = browser.defaultBrowserContext();
+        context.overridePermissions(`https://${region}.op.gg`, ["geolocation", "notifications"]);
+        const page = await browser.newPage();
+        if( region === 'kr'){
+             await page.goto(`https://www.op.gg/summoner/userName=${user}`)
+        }else{
+             await page.goto(`https://${region}.op.gg/summoner/userName=${user}`)
+        }
+
+        if (refresh === true) {
+            
+            await page.click('.ejbh9aw1') //update button
+            page.on('dialog', async dialog => {
+                await dialog.accept();
+            });
+            await page.waitForSelector('exlvoq30').catch(() => { return "player haven't played recently" }) //game list
+            await page.waitForTimeout(2000);
+        } else { }
+
+        await page.waitForTimeout(1000);
+
+        const level = await page.$eval('.level', e => e.innerText);
+        const image = await page.$eval('.profile-icon img', e => e.src);
+        const recentlyPlayedWith = await page.$$eval('.css-19xo3qz > table:nth-child(2) > tbody:nth-child(3) > tr', rows => {
+            return Array.from(rows, row => {
+              const columns = row.querySelectorAll('td');
+              return Array.from(columns, column => column.innerText);
+            });
+          }).catch(() => { return 'none' });
+
+        recentlyPlayedWith.unshift(['Summoner', 'Played', 'Win', 'Lose', 'Win Ratio']);
+
+        const stats = {
+            'Name': user,
+            'Level': level,
+            'ProfilePic': image,
+            'RecentlyPlayedWith': recentlyPlayedWith
+        }
+        await browser.close();
+        return stats
+    } catch (e) {
+        console.error(e)
+    }
+
+}
